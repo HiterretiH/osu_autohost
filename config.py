3d676ea5c0b94807d2c388937543a15fe4694259
HiterretiH <47186981+HiterretiH@users.noreply.github.com>
2021-08-06 11:32:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 lobby_name = "5*-6* auto host rotate | !info !queue"
-lobby_password = ""
+lobby_password = ""  # Leave blank for no password
 
-osuirc_name = ""
-osuirc_password = ""
+osuirc_name = ""  # Just your osu username
+osuirc_password = ""  # It's different from your account password. Get it here: https://osu.ppy.sh/p/irc
 
 help_msg = "This is an auto host rotate lobby. This means that host status automatically change between players, so everyone can set the map they want to play. [https://github.com/HiterretiH/osu_autohost src]"
 commands_timeout = 5
