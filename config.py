f706be1321a13c9033a917d670694cb04c54efc2
HiterretiH <47186981+hiterretih@users.noreply.github.com>
2022-02-04 08:39:41 +0300

    Update help message

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 osuirc_name = ""  # Just your osu username
 osuirc_password = ""  # It's different from your account password. Get it here: https://osu.ppy.sh/p/irc
 
-help_msg = "This is an auto host rotate lobby. This means that host status automatically changes between players, so everyone can set the map which want to play on. Available chat commands: !info !queue !skip (source)[https://github.com/HiterretiH/osu_autohost]"
+help_msg = "Available chat commands: !info !queue !skip. This is an auto host rotate lobby. This means that host status automatically changes between players, so everyone can set the map which want to play on. If you have any troubles or suggestions about the bot work, feel free to pm me; (source)[https://github.com/HiterretiH/osu_autohost]"
 commands_timeout = 5
 skip_percent = 51
